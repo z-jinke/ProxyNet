58632160eaa9dcace81373a72b8c11707e688ee7
Z <155460219+z-jinke@users.noreply.github.com>
2025-08-04 17:05:32 +0800

    更新IPInfo.js

--- Js/IPInfo.js
+++ Js/IPInfo.js
@@ -6,7 +6,7 @@ let body = $response.body;
 let obj = JSON.parse(body);
 
 let title = " ";
-let subtitle = (obj["ip"] || "") + " 国家:" + (obj["country_code"] || "");
+let subtitle = "IP:" (obj["ip"] || "") + " 国家:" + (obj["country_code"] || "");
 let ip = obj["ip"] || "";
 
 $done({ title, subtitle, ip });
