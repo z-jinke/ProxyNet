29db5c0f2f2fe300e9c5088f3c0074e13feb06e0
Z <155460219+z-jinke@users.noreply.github.com>
2025-08-10 20:55:33 +0800

    更新IPInfo.js

--- Js/IPInfo.js
+++ Js/IPInfo.js
@@ -5,9 +5,30 @@ if ($response.statusCode != 200) {
 var body = $response.body;
 var obj = JSON.parse(body);
 
+// 国家代码与国家名称的映射表
+const countryMap = {
+  "TW": "台湾",
+  "MO": "澳门",
+  "HK": "香港",
+  "US": "美国",
+  "JP": "日本",
+  "SG": "新加坡",
+  "DE": "德国",
+  "FR": "法国",
+  "GB": "英国",
+  "CA": "加拿大",
+  "AU": "澳大利亚",
+  "KH": "柬埔寨",
+  "VN": "越南",
+  "TH": "泰国",
+  "IN": "印度"
+};
+
+var ip = obj["ip"] || "未知";
+var countryCode = obj["country"] || "未知";
+var countryName = countryMap[countryCode] || "未知"; 
+
 var title = " ";
-var subtitle = (obj["ip"] || "") + " 国家:" + (obj["country_code"] || "");
-var ip = obj["ip"] || "";
+var subtitle = `IP-${ip} 地区:${countryName}`;
 
 $done({ title, subtitle, ip });
-
